9755ded3ac0a169af41165b0cb8f4c192422b2d8
YummyOreo <bobgim20@gmail.com>
2023-01-06 16:00:46 -0600

    adds rss

---
+++ create-rss/index.js
@@ -0,0 +1,39 @@
+import { parse } from 'node-html-parser';
+import RSS from 'rss';
+
+var feed = new RSS({
+    title: "YummyOreo's blog posts",
+    site_url: "https://oreo.is-a.dev",
+    feed_url: "https://oreo.is-a.dev/rss"
+});
+
+import fs from 'fs';
+var files = fs.readdirSync('../blogs/');
+
+function make_file() {
+    let xml = feed.xml({indent: true})
+
+    fs.writeFile("..\\rss", xml, (err) => {console.log(err)})
+}
+
+let done_items = 0;
+files.forEach((file, index) => {
+    fs.readFile("../blogs/" + file, "utf8", (_, data) => {
+        const root = parse(data);
+        let title = root.querySelector(".title h1").textContent;
+        let content = root.querySelector(".content");
+        let date = root.querySelectorAll(".date").textContent
+
+        feed.item({
+            title,
+            description: content.toString(),
+            url: "https://oreo.is-a.dev/blogs/" + file,
+            date,
+        })
+        done_items += 1;
+        if (done_items == files.length) {
+            make_file();
+        }
+    })
+})
+
