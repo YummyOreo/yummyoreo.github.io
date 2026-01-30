ec2d403334df9c9db44e1d7e087e70e2f0b22c79
Quinn <quinn@quinnyates.me>
2026-01-29 19:48:39 -0600

    feat: ssnapping

--- posts/photos.js
+++ posts/photos.js
@@ -3,25 +3,36 @@ function registerSlideshow(id) {
 	let slideshow = document.getElementById(id);
 	let children = slideshow.children
 	let width = 0
+	let widthArray = []
 	let count = 0
 	for (const child of children) {
 		count += 1
 		width -= child.getBoundingClientRect().width
 		width -= rem
+		widthArray.push(child.getBoundingClientRect().width)
 	}
 	width += window.innerWidth
 	width -= rem
 	let height = count * 300
+
+	let rollingW = 0
+	let precents = []
+	for (const w of widthArray) {
+		rollingW += w + rem;
+		let precent = (rollingW - ((w + rem)/2)) / (-width);
+		precents.push(precent)
+	}
 
 	gsap.to("#" + id, {
 		scrollTrigger: {
 			trigger: "#" + id,
 			start: "center center",
 			end: "+=" + height + " top",
-			// markers: true,
 			scrub: true,
-			pin: true
+			pin: true,
+			snap: {snapTo: precents, delay: 0.25 }
 		},
+		ease: "none",
 		x: width
 	})
 }
