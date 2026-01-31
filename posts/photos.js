3aea84e500148255709dfd5f28ac5c4b670a2016
Quinn <quinn@quinnyates.me>
2026-01-30 18:55:28 -0600

    feat: more photos

--- posts/photos.js
+++ posts/photos.js
@@ -5,30 +5,15 @@ function registerSlideshow(id) {
 	let slideshow = document.getElementById(id);
 	let children = slideshow.children
 	let width = 0
-	let widthArray = []
 	let count = 0
 	for (const child of children) {
 		count += 1
 		width -= child.getBoundingClientRect().width
 		width -= rem
-		widthArray.push(child.getBoundingClientRect().width)
 	}
 	width += window.innerWidth
 	width -= rem
 	let height = count * 300
-
-
-	let snap = false;
-	if (!mobile) {
-		let rollingW = 0
-		let precents = []
-		for (const w of widthArray) {
-			rollingW += w + rem;
-			let precent = (rollingW - ((w + rem) / 2)) / (-width);
-			precents.push(precent)
-		}
-		snap = { snapTo: precents, delay: 0.25 }
-	}
 
 	gsap.to("#" + id, {
 		scrollTrigger: {
@@ -37,7 +22,6 @@ function registerSlideshow(id) {
 			end: "+=" + height + " top",
 			scrub: true,
 			pin: true,
-			snap: snap
 		},
 		ease: "none",
 		x: width
@@ -47,4 +31,5 @@ function registerSlideshow(id) {
 window.onload = (e) => {
 	registerSlideshow("slideshow-1")
 	registerSlideshow("slideshow-2")
+	registerSlideshow("slideshow-3")
 }
